d987e50a7af4be0fb609e9464a995a622552242e
anthonyhales <143219954+anthonyhales@users.noreply.github.com>
2025-12-15 20:48:52 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -55,8 +55,14 @@ templates = Jinja2Templates(
 # Database
 # -------------------------------------------------
 def db():
-    conn = sqlite3.connect(DB_PATH, check_same_thread=False)
+    conn = sqlite3.connect(
+        DB_PATH,
+        check_same_thread=False,
+        timeout=30
+    )
     conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA journal_mode=WAL;")
+    conn.execute("PRAGMA synchronous=NORMAL;")
     return conn
 
 
